46b9dada6ede5198230926f2d2090fe09d9f1c1d
Jgzhu <jgz900306@gmail.com>
2016-11-21 11:07:14 +0800

    初始化

---
+++ DouYUZhiBo-Swift/DouYUZhiBo-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DouYUZhiBo-Swift
+//
+//  Created by 江贵铸 on 2016/11/21.
+//  Copyright © 2016年 江贵铸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
